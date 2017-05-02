fea6ee2b84ab44bd918d701173db803c210475aa
Michele Da Rin <michidarin@gmail.com>
2017-05-02 14:44:42 +0200

    Sistemato component tabella

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -9,7 +9,7 @@ import { DatabaseService } from '../../services/database.service';
 })
 export class TableComponent implements OnInit {
   results: any[];
-  
+
   constructor(
     private db: DatabaseService
   ) { }
@@ -26,6 +26,7 @@ export class TableComponent implements OnInit {
   }
 
   query () {
+    this.results = null;
     this.db
 			.query("SELECT DATE(ts_sens) AS `data`, id_box, id_sens, SUM(beein) AS `in`, SUM(beeout) AS `out` from beecounter GROUP BY `data`, id_box, id_sens")
 			.then((results: any[]) => {
