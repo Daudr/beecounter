21ae4c403a2178887e31739b118f5854d7917894
Michele Da Rin <michidarin@gmail.com>
2017-05-04 14:42:28 +0200

    Rimosse cose inutilizzate

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 const mysql = require("mysql");
 const cors = require("cors");
-const fs = require("fs");
-const timeout = require("connect-timeout");
 
 var app = express();
 app.use(bodyParser.json());
@@ -11,26 +9,6 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 app.use(cors());
-app.use(timeout('30s'));
-
-app.post("/api/connect", (req, res, next) => {
-  const user = req.body.username;
-  const password = req.body.password;
-
-  console.log(req.body);
-
-  connection = mysql.createConnection({
-    host     : process.env.RDS_HOST || 'localhost',
-    user     : process.env.RDS_USER || 'root',
-    password : process.env.RDS_PASSWORD || '',
-    database : process.env.RDS_DB || 'iot'
-  });
-
-  connection.connect(() => {
-    res.json({success: true});
-  });
-  console.log('connected to MySQL DB');
-});
 
 app.get("/api/query", (req, res, next) => {
 
@@ -140,13 +118,6 @@ app.get("/api/graph", (req, res, next) => {
 
   connection.end();
 });
-
-/*app.post("/api/close", (req, res) => {
-  connection.end(() => {
-    res.send({closed: true});
-  });
-  console.log('connection ended');
-})*/
 
 app.get('*', function(req, res, next){
 	res.sendFile(distDir + '/index.html');
