c1c78a296a862e346eb02ef74cd33bf5967c066e
Michele Da Rin <michidarin@gmail.com>
2017-05-26 23:11:30 +0200

    database service configurato per locale

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -12,64 +12,64 @@ export class DatabaseService {
 
 
   query (): Promise<any[]> {
-    // return this.http.get('http://localhost:8080/api/query')
-    return this.http.get('api/query')
+    return this.http.get('http://localhost:8080/api/query')
+    // return this.http.get('api/query')
     .toPromise()
     .then(response => response.json() as any[])
     .catch(this.handleError);
   }
 
   singolaDataTabella (data: string): Promise<any[]> {
-    // return this.http.get('http://localhost:8080/api/query/' + data)
-    return this.http.get('api/query/' + data)
+    return this.http.get('http://localhost:8080/api/query/' + data)
+    // return this.http.get('api/query/' + data)
     .toPromise()
     .then(response => response.json() as any[])
     .catch(this.handleError);
   }
 
   intervalloDateTabella (datada: string, dataa: string): Promise<any[]> {
-    // return this.http.get('http://localhost:8080/api/query/' + datada + '/' + dataa)
-    return this.http.get('api/query/' + datada + '/' + dataa)
+    return this.http.get('http://localhost:8080/api/query/' + datada + '/' + dataa)
+    // return this.http.get('api/query/' + datada + '/' + dataa)
     .toPromise()
     .then(response => response.json() as any[])
     .catch(this.handleError);
   }
 
   arniaTabella (arnia: number) {
-    // return this.http.get('http://localhost:8080/api/arniat/' + arnia)
-    return this.http.get('api/arniat/' + arnia)
+    return this.http.get('http://localhost:8080/api/arniat/' + arnia)
+    // return this.http.get('api/arniat/' + arnia)
     .toPromise()
     .then(response => response.json() as any[])
     .catch(this.handleError);
   }
 
   sensoreTabella (sensore: number) {
-    // return this.http.get('http://localhost:8080/api/sensoret/' + sensore)
-    return this.http.get('api/sensoret/' + sensore)
+    return this.http.get('http://localhost:8080/api/sensoret/' + sensore)
+    // return this.http.get('api/sensoret/' + sensore)
     .toPromise()
     .then(response => response.json() as any[])
     .catch(this.handleError);
   }
 
   graph (): Promise<any[]> {
-    // return this.http.get('http://localhost:8080/api/graph')
-    return this.http.get('api/graph')
+    return this.http.get('http://localhost:8080/api/graph')
+    // return this.http.get('api/graph')
     .toPromise()
     .then(response => response.json() as any[])
     .catch(this.handleError);
   }
 
   singolaDataGrafico (data: string): Promise<any[]> {
-    // return this.http.get('http://localhost:8080/api/graph/' + data)
-    return this.http.get('api/graph/' + data)
+    return this.http.get('http://localhost:8080/api/graph/' + data)
+    // return this.http.get('api/graph/' + data)
     .toPromise()
     .then(response => response.json() as any[])
     .catch(this.handleError);
   }
 
   intervalloDateGrafico (datada: string, dataa: string): Promise<any[]> {
-    // return this.http.get('http://localhost:8080/api/graph/' + datada + '/' + dataa)
-    return this.http.get('api/graph/' + datada + '/' + dataa)
+    return this.http.get('http://localhost:8080/api/graph/' + datada + '/' + dataa)
+    // return this.http.get('api/graph/' + datada + '/' + dataa)
     .toPromise()
     .then(response => response.json() as any[])
     .catch(this.handleError);
